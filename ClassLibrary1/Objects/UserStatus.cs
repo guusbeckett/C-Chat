9c669ebd5dc04ceec3821d8839ec571139496afa
Guus Beckett <ghbeckett@gmail.com>
2013-10-25 21:05:46 +0200

    Allowing user to change status

---
+++ ClassLibrary1/Objects/UserStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CChat_Library.Objects
+{
+    [Serializable]
+    public class UserStatus
+    {
+        public enum Status
+        {
+            STATUS_OFFLINE,
+            STATUS_ONLINE,
+            STATUS_AWAY,
+            SATUS_BUSY
+        }
+        public Status stat { get; set; }
+    }
+}
